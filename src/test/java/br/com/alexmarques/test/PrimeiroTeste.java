19196924b945dea5e8cbf84f2b6639b6e25dfe5d
Alex Marques <alexmarques2003@gmail.com>
2022-04-11 21:25:02 -0300

    Correção do mockito

--- src/test/java/br/com/alexmarques/test/PrimeiroTeste.java
+++ src/test/java/br/com/alexmarques/test/PrimeiroTeste.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
 @ExtendWith(MockitoExtension.class)
 public class PrimeiroTeste {
+
+    @Mock
     Calculadora calculadora;
+
     int numero1 = 10, numero2 = 5;
 
     @BeforeEach
